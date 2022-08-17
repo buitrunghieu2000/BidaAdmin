a9ce30f15ad6dde1d47ff5b5774f018f2f3d5f56
Thien Dao <dtrithien@tma.com.vn>
2022-08-17 17:08:17 +0700

    dashboard update

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -9,8 +9,8 @@ const Widget = (props: Props) => {
     <div className='p-[24px] rounded  h-[161px] bg-white relative'>
         <div></div>
         <div className='flex justify-between'>
-            <div className="w-[30px] h-[20px] object-cover"><img src={cart} alt="" /></div>
-            <div className="bg-green-500 rounded-[9999px] py-[2px] px-[10px] text-white text-[12px]">12% ^</div>
+            <div className="w-[30px] h-[20px] object-cover"><i className="fa-solid fa-cart-flatbed text-[20px] text-purple-300"></i></div>
+            <div className="bg-green-500 rounded-[9999px] py-[2px] px-[10px] text-white text-[12px]">12% <i className="fa-solid fa-arrow-up-wide-short"></i></div>
         </div>
         <div className="absolute bottom-[24px]">
             <h1 className='text-[24px] font-[700]'>9527</h1>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Widget from "../components/Widget/Widget";
-
+import hero from "../assets/img/happy.svg"
 type Props = {};
 
 const Dashboard = (props: Props) => {
@@ -12,7 +12,28 @@ const Dashboard = (props: Props) => {
       <Widget/> 
       <Widget/> 
       </div>
-   
+      <div className="hero grid grid-cols-4 gap-4 mt-[25px] ">
+        <div className="col-span-2 card bg-teal-400 border-teal-400 shadow-md text-white rounded h-[208px] p-[24px]">
+          <div className="card-body flex flex-row justify-center">
+            <div className="w-[160px] h-[133px]">
+              <img src={hero} alt="" />
+            </div>
+            <div className="py-2 ml-10">
+                <h1 className="h6">Good Job, Bida!</h1>
+                <p className="text-white text-xs">You've finished all of your tasks for this week.</p>
+
+                <ul className="mt-4">
+                    <li className="text-sm font-light"><i className="fad fa-check-double mr-2 mb-2"></i> Finish Dashboard Design</li>
+                    <li className="text-sm font-light"><i className="fad fa-check-double mr-2 mb-2"></i> Fix Issue #74</li>
+                    <li className="text-sm font-light"><i className="fad fa-check-double mr-2"></i> Publish version 1.0.6</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-2">
+          
+        </div>
+      </div>
     </div>
   );
 };
