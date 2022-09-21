d4dde1a5ef4d07a23a7e24cb578a88c2ac77d62d
Bui Trung Hieu <stevenlee01677797534@gmail.com>
2022-09-21 22:43:16 +0700

    carousel

--- src/pages/Product/Updateproduct.tsx
+++ src/pages/Product/Updateproduct.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import { useParams } from "react-router-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-type Props = {}
+type Props = {};
 
 const Updateproduct = (props: Props) => {
-    const {productId} = useParams()
+  const { productId } = useParams();
   return (
-    <div>{productId}</div>
-  )
-}
+    <div className="flex gap-8">
+      <Carousel width={500}>
+        <div>
+          <img src="https://cdn.tgdd.vn/Products/Images/1942/274763/android-sony-4k-43-inch-kd-43x80k-240522-030219-550x340.jpg" />
+          {/* <p className="legend">Legend 1</p> */}
+        </div>
+        <div>
+          <img src="https://cdn.tgdd.vn/Products/Images/1942/274763/android-sony-4k-43-inch-kd-43x80k-240522-030219-550x340.jpg" />
+          {/* <p className="legend">Legend 2</p> */}
+        </div>
+        <div>
+          <img src="https://cdn.tgdd.vn/Products/Images/1942/274763/android-sony-4k-43-inch-kd-43x80k-240522-030219-550x340.jpg" />
+          {/* <p className="legend">Legend 3</p> */}
+        </div>
+      </Carousel>
+      <div>content </div>
+    </div>
+  );
+};
 
-export default Updateproduct
+export default Updateproduct;
